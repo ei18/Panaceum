e9bdfb2e8a2401ced02c77d38efeff2477ab9e71
Keity <keityortegam@gmail.com>
2024-05-06 18:07:50 -0500

    Se crean las entidades y repositorios de administrador y paciente

--- src/main/java/com/riwi/panaceum/domain/repositories/PatientRepository.java
+++ src/main/java/com/riwi/panaceum/domain/repositories/PatientRepository.java
@@ -3,8 +3,8 @@ package com.riwi.panaceum.domain.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.riwi.panaceum.domain.entities.Administrator;
+import com.riwi.panaceum.domain.entities.Patient;
 
 @Repository
-public interface AdministratorRepository extends JpaRepository<Administrator, Long>{
+public interface PatientRepository extends JpaRepository<Patient, String>{
 }
