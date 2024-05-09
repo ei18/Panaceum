9794824d9d656af65a57fa98c8c2d756ef9a708a
LuisaP28 <respaldoluisap@gmial.com>
2024-05-09 15:08:05 -0500

    Creación de repositorios para las entidades de treatment, medication y treatmentHasMedication

--- src/main/java/com/riwi/panaceum/api/dto/response/TreatmentHasMedicationResponse.java
+++ src/main/java/com/riwi/panaceum/api/dto/response/TreatmentHasMedicationResponse.java
@@ -1,7 +1,5 @@
 package com.riwi.panaceum.api.dto.response;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TreatmentHasMedicationResponse {
-    private Long treatmentsId;
-    private Long medicationsId;
-    private List<TreatmentResponse> treatments;
-    private List<MedicationResponse> medications;
+    private TreatmentResponse treatment;
+    private MedicationResponse medication;
 }
