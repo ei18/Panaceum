a7a3e9997734cce1b960f50c8c7f61fdd49a5de9
LuisaP28 <respaldoluisap@gmial.com>
2024-05-19 19:23:32 -0500

    Se implementa otro método en errors

--- src/main/java/com/riwi/panaceum/api/error_handler/BadRequestController.java
+++ src/main/java/com/riwi/panaceum/api/error_handler/BadRequestController.java
@@ -3,7 +3,6 @@ package com.riwi.panaceum.api.error_handler;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,32 +10,36 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.riwi.panaceum.api.dto.errors.BaseErrorResponse;
+import com.riwi.panaceum.api.dto.errors.ErrorResp;
 import com.riwi.panaceum.api.dto.errors.ErrorsResp;
+import com.riwi.panaceum.utils.exceptions.IdNotFoundException;
 
 @RestControllerAdvice
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class BadRequestController {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public BaseErrorResponse handleBadRequest(MethodArgumentNotValidException exception){
+    @ExceptionHandler(IdNotFoundException.class)
+    public BaseErrorResponse handleIdNotFound(IdNotFoundException exception){
 
+        return ErrorResp.builder()
+        .message(exception.getMessage())
+        .status(HttpStatus.BAD_REQUEST.name())
+        .code(HttpStatus.BAD_REQUEST.value())
+        .build(); 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseErrorResponse handleErrors(MethodArgumentNotValidException exception){
         List<String> errors = new ArrayList<>();
 
         exception.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
 
-        return ErrorsResp.builder().code(HttpStatus.BAD_REQUEST.value()).status(HttpStatus.BAD_REQUEST.name()).errors(errors).build();
-    }
-
-    @ExceptionHandler(BadRequestException.class)
-    public BaseErrorResponse badRequest(BadRequestException exception){
-        List<String> errors = new ArrayList<>();
-
-        errors.add(exception.getMessage());
-
         return ErrorsResp.builder()
-            .code(HttpStatus.BAD_REQUEST.value())
-            .status(HttpStatus.BAD_REQUEST.name())
-            .errors(errors)
-            .build();
+        .errors(errors)
+        .status(HttpStatus.BAD_REQUEST.name())
+        .code(HttpStatus.BAD_REQUEST.value())
+        .build();
     }
+    
 }
+
